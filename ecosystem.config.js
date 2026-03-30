56a9b4c69d0dc0387db8b401f554b12443bbf29e
MaheshaDinu98 <maheeshadinushan@gmail.com>
2026-03-31 02:24:25 +0530

    small typo fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,21 +11,21 @@ module.exports = {
       script : "java",
       args   : "-jar ./catalog-service/target/catalog-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/catalog-service.log",
-      instences: 2
+      instances: 2
     },
     {
       name   : "media-service",
       script : "java",
       args   : "-jar ./media-service/target/media-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/media-service.log",
-      instences: 2
+      instances: 2
     },
     {
       name   : "user-service",
       script : "java",
       args   : "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/user-service.log",
-      instences: 2
+      instances: 2
     }
 ]
 }
